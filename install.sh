8996e42303e8eeb1f7c3b3fcc6b5451bab8ecc50
touchedthecode <git@touchedthecode.com>
2023-12-25 12:04:58 +0100

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+create_link() {
+	source=$1
+	target=$2
+	ln -sfn $source $target
+	if [ $? -eq 0 ]; then
+		echo "Added link $source -> $target"
+	else
+		echo "Error adding link $source -> $target"
+	fi
+}
+
 # zsh
-ln -sf $HOME/.dotfiles/.zshrc $HOME/.zshrc
+create_link $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # powerlevel10k
-ln -sf $HOME/.dotfiles/.p10k.zsh $HOME/.p10k.zsh
+create_link $HOME/.dotfiles/.p10k.zsh $HOME/.p10k.zsh
 
 # nvim
-mkdir -p $HOME/.config/nvim/
-ln -sf $HOME/.dotfiles/.config/nvim/init.vim $HOME/.config/nvim/init.vim
+create_link $HOME/.dotfiles/.config/nvim $HOME/.config
